52009c1db1dfcfdcc7afa16e8e1e54f50d290623
Aaron Straup Cope <straup@gmail.com>
2009-08-03 22:28:12 -0700

    use cut instead of awk to parse PHOTO_AND_STUFF

--- uploadbyemail.sh
+++ uploadbyemail.sh
@@ -4,22 +4,24 @@ DATE=`date`
 echo "[start] ${DATE} reading ${FLICKR_CFG}"
 
 PHOTO_AND_STUFF=`cat -u /dev/stdin | ube-receivemail.pl`
-echo "[parse] ${PHOTO_AND_STUFF}"
+echo "[parse] '${PHOTO_AND_STUFF}'"
 
-# please to make me bettar...
+PHOTO=`echo ${PHOTO_AND_STUFF} | cut -f 1 -d"|"`
+PROCESS=`echo ${PHOTO_AND_STUFF} | cut -f 2 -d"|"`
+EXTRA=`echo ${PHOTO_AND_STUFF} | cut -f 3 -d "|"`
+TITLE=`echo ${PHOTO_AND_STUFF} | cut -f 4 -d "|"`
+PERMS=`echo ${PHOTO_AND_STUFF} | cut -f 5 -d "|"`
 
-PHOTO=`echo ${PHOTO_AND_STUFF} | awk '{split($1, parts, " "); print parts[1]; }'`
-PROCESS=`echo ${PHOTO_AND_STUFF} | awk '{split($2, parts, " "); print parts[1]; }'`
-EXTRA=`echo ${PHOTO_AND_STUFF} | awk '{split($3, parts, " "); print parts[1]; }'`
-
-echo "[photo] ${PHOTO}"
-echo "[process] ${PROCESS}"
-echo "[extra] ${EXTRA}"
+echo "[photo] '${PHOTO}'"
+echo "[process] '${PROCESS}'"
+echo "[extra] '${EXTRA}'"
+echo "[title] '${TITLE}'"
+echo "[perms] '${PERMS}'"
 
 FILTRD=`ube-processphoto.sh ${PHOTO} ${FILTR} ${PROCESS}`
 echo "FILTRD) ${FILTRD}"
 
-ID=`ube-postphoto.pl -c ${FLICKR_CFG} -i ${FILTRD} -p ${PROCESS}`
+ID=`ube-postphoto.pl -c ${FLICKR_CFG} -i ${FILTRD} -p ${PROCESS} -T "${TITLE}" -P ${PERMS}`
 echo "[id] ${ID}"
 
 if [ -f ${FILTRD} ]; then
